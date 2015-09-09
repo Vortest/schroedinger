c09556d51790b98376aa0983add7bc4eea79fe6d
Brian Kitchener <kitchener.brian@gmail.com>
2015-09-08 18:20:39 -0600

    getting api tests working in cci

--- api/tests/posts.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_one(self):
-    assert 1 == 2
--- api/tests/test_allPosts.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-__author__ = 'Brian'
-
-
-class TestAllPosts(TestCase):
-  def test_get(self):
-    self.fail()
