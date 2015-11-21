cedc0ee17ccc71c2b939a0ab95974d0ab9535afb
Brian Kitchener <kitchener.brian@gmail.com>
2015-11-21 10:18:25 -0700

    fixing sauce

--- app/tests/test_browser_session.py
+++ app/tests/test_browser_session.py
@@ -15,7 +15,7 @@ class TestBrowserSession(unittest.TestCase):
         session = BrowserSession()
         session.browser = "Firefox"
         session.sauce_user = "bkitchener1"
-        session.sauce_key = ""
+        session.sauce_key = "c479e821-57e7-4b3f-8548-48e520585187"
         session.use_defaults(webdriver.DesiredCapabilities.FIREFOX)
         session.start_sauce_session()
         session.driver.get("http://www.google.com")
