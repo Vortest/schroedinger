b441abbb1f4b05ab7a3617142a604e0406619743
Brian Kitchener <kitchener.brian@gmail.com>
2015-11-22 10:12:10 -0700

    fixing crawler queue

--- app/tests/test_crawler_queue.py
+++ app/tests/test_crawler_queue.py
@@ -5,17 +5,18 @@ from app.crawler_consumer import CrawlerConsumer
 from app.crawler_producer import CrawlerProducer
 from models.crawler_task import CrawlerTask
 import logging
+from models.status import Status
 
 pika_logger = logging.getLogger('pika')
 pika_logger.setLevel(logging.CRITICAL)
 
 class TestCrawlerQueue(unittest.TestCase):
     def test_crawler(self):
-        task1 = CrawlerTask(status=CrawlerTask.Status.QUEUED, message="This is a message", user="12345", url="http://www.google.com/")
+        task1 = CrawlerTask(status=Status.QUEUED, message="This is a message", user="12345", url="http://www.google.com/")
         task1.save()
-        task2 = CrawlerTask(status=CrawlerTask.Status.FAILED, message="This is a message", user="12345", url="http://www.failed.com/")
+        task2 = CrawlerTask(status=Status.FAILED, message="This is a message", user="12345", url="http://www.failed.com/")
         task2.save()
-        task3 = CrawlerTask(status=CrawlerTask.Status.QUEUED, message="This is a message", user="12345", url="http://www.baidu.com/", start_after = datetime.datetime.now() + datetime.timedelta(seconds=10))
+        task3 = CrawlerTask(status=Status.QUEUED, message="This is a message", user="12345", url="http://www.baidu.com/", start_after = datetime.datetime.now() + datetime.timedelta(seconds=10))
         task3.save()
         producer = CrawlerProducer(1)
         consumer = CrawlerConsumer()
